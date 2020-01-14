4bd8e13c217ea313d736571de1e3240f9a462965
e.alihangt@gmail.com <e.alihangt@gmail.com>
2020-01-14 20:31:44 +0600

    переделывание

--- ServerResposeApp/Program.cs
+++ ServerResposeApp/Program.cs
@@ -23,51 +23,29 @@ namespace ServerResposeApp
                 }";
             var listener = new TcpListener(IPAddress.Parse("10.1.4.82"), 3231);
             listener.Start();
-            Console.WriteLine("Сервер запущен");
             while (true)
             {
                 using (var client = listener.AcceptTcpClient())
                 {
-                    Console.WriteLine("Входящее соединение");
                     using (var stream = client.GetStream())
                     {
                         var resText = String.Empty;
                         while (stream.DataAvailable)
                         {
                             var buffer = new byte[1024];
-                            stream.Read(buffer, 0, buffer.Length);
-                            resText += System.Text.Encoding.UTF8.GetString(buffer);
+                            await stream.ReadAsync(buffer, 0, buffer.Length);
+                            resText += System.Text.Encoding.UTF8.GetString(buffer);                            
                         }
                         var obj = JObject.Parse(json);
                         if (String.Equals((string)obj["Path"], "/user/"))
                         {
                             var data = JsonConvert.DeserializeObject<Request<User>>(json);
-                            using (var context = new Context())
-                            {
-                                switch (data.Action)
-                                {
-                                    case "Give": 
-                                                                               
-                                        break;
-                                    case "Add":
-                                        context.Add<User>(data.Add);
-                                        break;
-                                    case "Update":                                         
-                                        context.Add<User>(data.Update);
-                                        break;
-                                    case "Remove": 
-                                        context.Add<User>(data.Remove);
-                                        break;
-                                }
-                                await context.SaveChangesAsync();
-                            }
                         }
                         Console.WriteLine($"Данные от клиента - {resText}");
                         var answerData = System.Text.Encoding.UTF8.GetBytes("Запрос получен");
                         stream.Write(answerData, 0, answerData.Length);
                     }
                 }
-                Console.WriteLine("Соединение закрыто");
             }
         }
 
